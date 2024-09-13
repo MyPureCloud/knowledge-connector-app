5f27b8dd51bfd64d44a3fbb67c8fc397964b7606
Balazs Simon <balazs.simon@genesys.com>
2024-09-13 07:09:27 +0200

    Changing type from any to unknown

--- src/adapter/errors/api-error.ts
+++ src/adapter/errors/api-error.ts
@@ -2,7 +2,7 @@ import { ErrorCodes } from '../../utils/errors/error-codes.js';
 import { ErrorBasePublic } from '../../utils/errors/error-base-public.js';
 
 export class ApiError extends ErrorBasePublic {
-  constructor(message: string, details: { [key: string]: any }) {
+  constructor(message: string, details: { [key: string]: unknown }) {
     super(ErrorCodes.THIRD_PARTY_UNEXPECTED_ERROR, message, details);
   }
 }
